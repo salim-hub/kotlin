8ed03832350320529ad244c887d3af1d05044262
salim-hub <62649383+salim-hub@users.noreply.github.com>
2021-11-24 02:34:43 +0300

    Add files via upload

---
+++ FirebaseAuthDemo/app/src/main/java/com/example/firebaseauthdemo/LoginActivity.kt
@@ -0,0 +1,86 @@
+package com.example.firebaseauthdemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tv_register.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
+        }
+
+        btn_login.setOnClickListener {
+            when {
+                TextUtils.isEmpty(et_login_email.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please enter email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                TextUtils.isEmpty(et_login_password.text.toString().trim { it <= ' ' }) -> {
+                    Toast.makeText(
+                        this@LoginActivity,
+                        "Please Enter Password",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                else -> {
+                    val email: String = et_login_email.text.toString().trim { it <= ' ' }
+                    val password: String = et_login_password.text.toString().trim { it <= ' ' }
+
+                    // Log-In using FirebaseAuth
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(
+                            OnCompleteListener<AuthResult> { task ->
+                                // If the registration is successfully done
+                                if (task.isSuccessful) {
+                                    val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        "You are logged in successfully.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val intent =
+                                        Intent(this@LoginActivity, MainActivity::class.java)
+                                    intent.flags =
+                                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                    intent.putExtra(
+                                        "user_id",
+                                        FirebaseAuth.getInstance().currentUser!!.uid
+                                    )
+                                    intent.putExtra("email_id", email)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                                else {
+                                    Toast.makeText(
+                                        this@LoginActivity,
+                                        task.exception!!.message.toString(),
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                }
+                            }
+                        )
+                }
+            }
+        }
+    }
+}
